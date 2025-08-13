84da86666f823416d2d703edb2538df66e315b5d
amalozzi300 <andrewmalozzi900@gmail.com>
2025-08-13 16:24:41 -0400

    starting fresh -- new user/profile approach

--- footballpool/footballpool/settings.py
+++ footballpool/footballpool/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'localflavor',
     'phone_field',
     # custom apps
-    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,5 +124,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'users.User'
