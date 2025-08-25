cf418f515c13cb7150b0ed1a693fd8738cb54090
amalozzi300 <andrewmalozzi900@gmail.com>
2025-08-25 14:42:10 -0400

    slight tweaks to accomodate moving app folder locations

--- footballpool/footballpool/profiles/apps.py
+++ footballpool/footballpool/profiles/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'profiles'
+    name = 'footballpool.profiles'
 
     def ready(self):
         import footballpool.profiles.signals
--- footballpool/footballpool/urls.py
+++ footballpool/footballpool/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('profiles.urls')),
+    path('', include('footballpool.profiles.urls')),
 
     path('admin/', admin.site.urls),
 ]
